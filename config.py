602ecaed4bfe07b5b5e29f381c1d5a5688dc6e47
Abbas <112128172+abbas2come001@users.noreply.github.com>
2023-07-05 21:31:35 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+import os
+APP_ID = os.environ.get("APP_ID")
+APP_HASH = os.environ.get("APP_HASH")
+
+session1 = os.environ.get("TERMUX")
+SESSION1 = os.environ.get("TERMUX")
+DEVLOO = os.environ.get("DEVLO")
+CHNA = os.environ.get("CHNA")
+UPSTREAM_REPO = os.environ.get(
+        "UPSTREAM_REPO", "https://github.com/abbas2come001/shtg.git"
+)
+shahm1 = TelegramClient(StringSession(session1), APP_ID, APP_HASH)
+
+
+ispay = ['yes']
+ispay2 = ['yes']
+
+
