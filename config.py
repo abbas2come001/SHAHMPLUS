a5d0466911d0f12c143cbb34cdf5fe873db6ae1d
Abbas <112128172+abbas2come001@users.noreply.github.com>
2023-07-05 21:56:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,6 @@ session1 = os.environ.get("TERMUX")
 SESSION1 = os.environ.get("TERMUX")
 DEVLOO = os.environ.get("DEVLO")
 CHNA = os.environ.get("CHNA")
-UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/abbas2come001/shtg.git"
-)
 shahm1 = TelegramClient(StringSession(session1), APP_ID, APP_HASH)
 
 
